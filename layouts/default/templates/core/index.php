3d32fc82be933388e26d126978084c94092335fd
Drew Strojny <dstrojny@jestro.com>
2011-09-14 18:30:28 +0100

    Move loop to a partials folder

--- layouts/default/templates/core/index.php
+++ layouts/default/templates/core/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div id="content" class="clear">
-	<?php get_template_part( '_loop' ); ?>
+	<?php get_template_part( 'loop' ); ?>
 </div><!--end content-->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
